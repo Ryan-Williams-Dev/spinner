0c68df5c6fc49738003fdfdbcfc6f3254147d3d5
Ryan Williams <r.d.williams.ca@gmail.com>
2022-02-15 05:47:02 +0000

    made it in a loop, it's a few lines of code longer becuase i wanted the number of spins to be a parameter

---
+++ spinner2.js
@@ -0,0 +1,17 @@
+const spinner = (rotations) => {
+  const spinSyms = ['\r|   ', '\r/   ', '\r-   ', '\r\\   '];
+  delay = 100;
+  for (let i = 0; i < (rotations * 2); i++) {
+    for (const sym of spinSyms) {
+      setTimeout(() => {
+        process.stdout.write(sym);
+        if (i === (rotations * 2) - 1) {
+          process.stdout.write('\r|   ');
+        }
+      }, delay)
+      delay += 200;
+    }
+  }
+}
+
+spinner(2);
